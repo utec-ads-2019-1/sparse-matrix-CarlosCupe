0e962a3fcd050dc26cef03dd77dac0502b3f044f
zero <carlos.cupe@utec.edu.pe>
2019-06-05 20:43:09 -0500

    first commit

--- matrix.h
+++ matrix.h
@@ -2,13 +2,17 @@
 #define SPARSE_MATRIX_MATRIX_H
 
 #include <stdexcept>
+#include <iostream>
+#include <vector>
+#include "node.h"
 
 using namespace std;
 
 template <typename T>
 class Matrix {
 private:
-    Node<T> *root;
+    vector<Node<T>*> vector_row;
+    vector<Node<T>*> vector_col;
     unsigned rows, columns;
 
 public:
@@ -20,6 +24,7 @@ public:
     Matrix<T> operator*(Matrix<T> other) const;
     Matrix<T> operator+(Matrix<T> other) const;
     Matrix<T> operator-(Matrix<T> other) const;
+    Matrix<T> operator=(Matrix<T> other) const;
     Matrix<T> transpose() const;
     void print() const;
 
@@ -27,3 +32,70 @@ public:
 };
 
 #endif //SPARSE_MATRIX_MATRIX_H
+
+
+template <typename T>
+Matrix<T>::Matrix(unsigned rows, unsigned columns) {    
+    this->rows = rows;
+    this->columns = columns;
+
+    for (unsigned i = 0; i < rows; ++i)
+        vector_row.push_back(new Header<T> (i));
+
+    for (unsigned i = 0; i < columns; ++i)
+        vector_col.push_back(new Header<T> (i));
+}
+
+template <typename T>
+void Matrix<T>::set(unsigned x, unsigned y, T value) {
+    if (x >= rows || y >= columns)
+        exit(1);
+    Element<T> **ptr_row = (Element<T>**)&(vector_row[x]->down);
+    Element<T> **ptr_col = (Element<T>**)&(vector_col[y]->next);
+
+    while (*ptr_row != nullptr) {
+        if ((*ptr_row)->y >= y) break;
+        ptr_row = (Element<T>**)&((*ptr_row)->down);
+    }
+
+    if ((*ptr_row)->y == y) {
+        // Crear funcion delete si value es 0
+        //if (value == 0) ;
+        //else (*ptr_row)->value = value;
+        return;
+    }
+
+    while (*ptr_col != nullptr) {
+        if ((*ptr_col)->x > x) break;
+        ptr_col = (Element<T>**)&((*ptr_col)->down);
+    }
+
+    Element<T> *temp = new Element<T>(x, y, value);
+    temp->down = (Node<T>*)*ptr_col;
+    temp->next = (Node<T>*)*ptr_row;
+
+    *ptr_col = temp;
+    *ptr_row = temp;
+}
+
+template <typename T>
+void Matrix<T>::print() const{
+    Element<T> ptr;
+    for (Node<T> row : vector_row) {
+        ptr = (Element<T>*)row->next;
+        for (unsigned i = 0; i < rows; ++i) {
+            if (ptr->value == i) {
+                cout << ptr->value << " ";
+                ptr = (Element<T>*)ptr->next;
+            } else
+                cout << 0 << " ";
+        }
+        cout << endl;
+    }
+}
+
+template <typename T>
+Matrix<T>::~Matrix() {
+    vector_col.clear();
+    vector_row.clear();
+}
--- node.h
+++ node.h
@@ -10,7 +10,31 @@ protected:
     Node<T> *next, *down;
 
 public:
-    explicit Node();
+    explicit Node() 
+    : next(nullptr), down(nullptr) {}
+
+    friend class Matrix<T>;
+};
+
+template <typename T>
+class Header : public Node<T> {
+protected:
+    unsigned index;
+
+public:
+    Header(unsigned __index__)
+    : index(__index__) { }
+};
+
+template <typename T>
+class Element : public Node<T> {
+protected:
+    unsigned x, y;
+    T value;
+
+public:
+    Element(unsigned __x__, unsigned __y__, T __value__)
+    : x(__x__), y(__y__), value(__value__) { }
 
     friend class Matrix<T>;
 };
